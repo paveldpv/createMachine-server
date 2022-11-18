7a9710fad4799da1436361c61b82006c3bce670f
pavel <paveldpv91@gmail.com>
2022-11-18 18:10:27 +0300

    not bot

--- config/telgramBot.js
+++ config/telgramBot.js
@@ -1,4 +1,4 @@
-const TOKEN   = `5184228479:AAHZtW__zjHKl_LwCbEpXDGkyFumtjX7LG0`
+const TOKEN   = `token`
 const RspkBot = require('node-telegram-bot-api')
 const bot     = new RspkBot(TOKEN, {polling:true})
 
